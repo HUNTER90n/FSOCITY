cfb2d36874b71c2a80c4a2eead4719abcd0b613a
HUNTER90n <se2490535@gmail.com>
2025-11-29 18:09:01 +0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,41 @@
+import time
+import webbrowser
+from colorama import Fore, Style
+import os
+
+print(Fore.RED + "Hello")
+print("WELCOME TO FSOCITY ENTRYING GATEWAY")
+time.sleep(2)
+print("FIRST REQUIRMENT IS TO FOLLOW US ON TIKTOK")
+time.sleep(3)
+print("HERE IS OUR TIKTOK ID")
+time.sleep(2)
+print("@dexter_the_king_69")
+webbrowser.open("https://www.tiktok.com/@dexter_the_king_69")
+url = "https://www.tiktok.com/@dexter_the_king_69"
+os.system(f'am start -a android.intent.action.VIEW -d "{url}"')
+print("VERFING PLEASE WAIT 30s")
+time.sleep(30)
+print("not completed")
+time.sleep(2)
+print("TRYING AGAIN IN 5s")
+for i in range(5, 0, -1):
+    print(f"[+] Countdown: {i}")
+    time.sleep(1)
+
+print("OPNING TIKTOK ID AGAIN")
+time.sleep(2) 
+webbrowser.open("https://www.tiktok.com/@dexter_the_king_69")
+url = "https://www.tiktok.com/@dexter_the_king_69"
+os.system(f'am start -a android.intent.action.VIEW -d "{url}"')
+print("VERFING AGAIN PLEASE WAIT 30s")
+time.sleep(30)
+print("LOADING FINAL STEP....")
+time.sleep(3)
+print("VERFIED NOW YOU CAN ENTER FSOCITY")
+time.sleep(3)
+print(Style.RESET_ALL + "WELCOME TO FSOCITY")
+time.sleep(2)
+webbrowser.open("https://whatsapp.com/channel/0029VbBfD6H1NCrYUGOLev2j")
+url = "https://whatsapp.com/channel/0029VbBfD6H1NCrYUGOLev2j"
+os.system(f'am start -a android.intent.action.VIEW -d "{url}"')
