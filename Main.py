8b0285223aba9e143b229044253b7b57ed921e85
HUNTER90n <se2490535@gmail.com>
2025-11-30 08:32:17 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,10 @@ import time
 import webbrowser
 from colorama import Fore, Style
 import os
+import pyfiglet
 
+banner = pyfiglet.figlet_format(Fore.GREEN + "FSOCIETY")
+print(banner)
 print(Fore.RED + "Hello")
 print("WELCOME TO FSOCITY ENTRYING GATEWAY")
 time.sleep(2)
@@ -10,11 +13,9 @@ print("FIRST REQUIRMENT IS TO FOLLOW US ON TIKTOK")
 time.sleep(3)
 print("HERE IS OUR TIKTOK ID")
 time.sleep(2)
-print("Also Like all videos")
-time.sleep(4)
 print("@fsocity_69")
-webbrowser.open("https://www.tiktok.com/@dexter_the_king_69")
-url = "https://www.tiktok.com/@dexter_the_king_69"
+webbrowser.open("https://www.tiktok.com/@fsocity_69")
+url = "https://www.tiktok.com/@fsocity_69"
 os.system(f'am start -a android.intent.action.VIEW -d "{url}"')
 print("VERFING PLEASE WAIT 30s")
 time.sleep(30)
@@ -27,8 +28,8 @@ for i in range(5, 0, -1):
 
 print("OPNING TIKTOK ID AGAIN")
 time.sleep(2) 
-webbrowser.open("https://www.tiktok.com/@dexter_the_king_69")
-url = "https://www.tiktok.com/@dexter_the_king_69"
+webbrowser.open("https://www.tiktok.com/@fsocity_69")
+url = "https://www.tiktok.com/@fsocity_69"
 os.system(f'am start -a android.intent.action.VIEW -d "{url}"')
 print("VERFING AGAIN PLEASE WAIT 30s")
 time.sleep(30)
@@ -37,7 +38,11 @@ time.sleep(3)
 print("VERFIED NOW YOU CAN ENTER FSOCITY")
 time.sleep(3)
 print(Style.RESET_ALL + "WELCOME TO FSOCITY")
-time.sleep(2)
 webbrowser.open("https://whatsapp.com/channel/0029VbBfD6H1NCrYUGOLev2j")
 url = "https://whatsapp.com/channel/0029VbBfD6H1NCrYUGOLev2j"
 os.system(f'am start -a android.intent.action.VIEW -d "{url}"')
+print(Fore.GREEN + "PROGRAM BY FSOCITY IS CLOSING IN 10s")
+for i in range(10, 0, -1):
+    print(f"[+] Closing in: {i}")
+    time.sleep(1)
+print(Style.RESET_ALL + "GOODBYE!")
